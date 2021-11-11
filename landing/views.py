2dca46772c221bdc6fc39b24ffb733c2f5df4099
CousinAvi <Washington_Capitals_1@mail.ru>
2021-11-11 13:11:35 +1000

    Update to new contracts

--- landing/views.py
+++ landing/views.py
@@ -22,58 +22,27 @@ with open("./contracts/AuctionBox.json") as f:
 
 
 def landing(request):
+    
     tokens_in_system = art_token.functions.totalSupply().call()
-    share_ids_in_system = [
-        art_token.functions.ids_external(i).call() for i in range(tokens_in_system)
-    ]
-    share_ids_in_system = list(set(share_ids_in_system))
+    
+    tokenIds = art_token.functions.getTokenIds().call()
+    tokensInSystem = len(tokenIds)
 
-    token_ids = [
-        art_token.functions.share_to_token(i).call() for i in share_ids_in_system
-    ]
-    token_ids = list(set(token_ids))
-    a = [art_token.functions.get_art_by_share_id(i).call() for i in share_ids_in_system]
+    infoAboutTokens = []
+    for index in tokenIds:
+        infoAboutTokens.append([index]+art_token.functions.getArtToken(index).call())
 
-    token_ids_in_system = [int(part[0]) for part in a]
+    links_for_tokens = [art_token.functions.tokenURI(index).call() for index in tokenIds]
+    print(links_for_tokens)
+    for i in range(len(infoAboutTokens)):
+        part = infoAboutTokens[i]
+        part.append('https://ipfs.io/ipfs/'+ links_for_tokens[i])
 
-    keys_list = [str(part) for part in token_ids_in_system]
-    values_list = share_ids_in_system
-    zip_iterator = zip(keys_list, values_list)
-    a_dictionary = dict(zip_iterator)
-
-    token_ids_in_system = list(set(token_ids_in_system))
-    tokens_in_system = len(token_ids_in_system)
-
-    info_about_tokens = []
-    for i in token_ids_in_system:
-        info_about_tokens.append(
-            art_token.functions.get_art_by_share_id(a_dictionary[str(i)]).call()
-        )
-
-    links_for_tokens = [
-        art_token.functions.get_link_by_token_id(int(i)).call()
-        for i in token_ids_in_system
-    ]
-
-    for i in range(len(info_about_tokens)):
-        part = info_about_tokens[i]
-        part.append("https://ipfs.io/ipfs/" + links_for_tokens[i])
-    code_names = [
-        "ID",
-        "Owner",
-        "Entity",
-        "Name",
-        "Author",
-        "License",
-        "Year",
-        "Orig",
-        "Extra",
-        "Link",
-    ]
+    code_names = ['ID', 'Entity', 'Name', 'Author', 'Year', 'Extra', 'Link']
 
     info_to_render = []
-    for info in info_about_tokens:
+    for info in infoAboutTokens:
         info_to_render.append(dict(zip(code_names, info)))
 
-    style = "color:#fff; background-color:#B22222"
-    return render(request, "artproject_owner/index.html", locals())
+    style = 'color:#fff; background-color:#B22222'
+    return render(request, 'artproject_owner/index.html', locals())
